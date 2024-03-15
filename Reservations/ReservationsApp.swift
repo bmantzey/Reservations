39315fb83a9ad876968d9e94cd92c6d34a727957
Brandon Mantzey <bmantzey@BrandonacStudio-9.lan>
2024-03-15 12:01:02 -0500

    Initial Commit.  Blank Project.

---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Brandon Mantzey on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
